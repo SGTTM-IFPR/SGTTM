7149a381b6a406e0eb0961810a7a7986906b13c8
Nicolas Farhat <nicolasfarhat7@gmail.com>
2023-04-03 20:48:41 -0300

    Operações do repositorio

--- back/Repositories/user_repository.py
+++ back/Repositories/user_repository.py
@@ -1,9 +1,33 @@
 from database import db
+from Models import user_model
 
 
 class UserRepository:
 
-    def create_user(self, user):
+    def create_user(self, user_data):
+        user = user_model(**user_data)
         db.session.add(user)
         db.session.commit()
-        return "deu certo"
+        return user
+
+    def get_all_users(self):
+        return user_model.query.all()   
+
+    def get_user_by_id(self, user_id):
+        return user_model.query.filter_by(id=user_id).first()
+
+    def update_user(self, user_id, user_data):
+        user = self.get_user_by_id(user_id)
+        if user:
+            for key, value in user_data.items():
+                setattr(user, key, value)
+            db.session.commit()
+            return user
+
+    def delete_user(self, user_id):
+        user = self.get_user_by_id(user_id)
+        if user:
+            db.session.delete(user)
+            db.session.commit()
+            return True
+        return False
--- back/app.py
+++ back/app.py
@@ -4,20 +4,19 @@ from Controllers.UserController import routes
 from Models.Enums.ConditionEnum import ConditionEnum
 from Models.Enums.SexEnum import SexEnum
 from Models.Enums.TournamentTypeEnum import TournamentTypeEnum
+from Models.user_model import *
 from flask_sqlalchemy import SQLAlchemy
 from Utils.db_utils import DbUtils
 from database import db
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://root:database@localhost/ifpr'
+#mudar endereço do banco, esse é o default pro pc do ifpr
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://root:bancodedados@localhost/tcc'
 app.register_blueprint(routes, url_prefix='/users')
 
 db.init_app(app)
 
 DbUtils.test_database_connection(app, db)
-
-from Models.user_model import *
-
 
 with app.app_context():
     db.create_all()
