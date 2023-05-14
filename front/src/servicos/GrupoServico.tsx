8bbd30b26d66ea61ea73baf425492f3441caa638
carlosquadros-br <carlos.quadros.br@gmail.com>
2023-05-14 14:50:09 -0300

    adicionado intercptor no grupo tbm

--- front/src/servicos/GrupoServico.tsx
+++ front/src/servicos/GrupoServico.tsx
@@ -1,6 +1,21 @@
 import axios, { AxiosHeaders } from "axios";
 import { GrupoData } from "../datas/GrupoData";
 
+axios.interceptors.request.use(
+    config => {
+      if (config.url?.includes('/login')) {
+          return config 
+        }
+      const token = localStorage.getItem('token')
+      if (token) {
+        config.headers.Authorization = `Bearer ${token}`
+      }
+      return config
+    },
+    error => {
+      return Promise.reject(error)
+    }
+  )
 
 export const getGruposByTorneioId = async (id: number): Promise<GrupoData[]> => {
     const baseURL = "http://localhost:5000/";
