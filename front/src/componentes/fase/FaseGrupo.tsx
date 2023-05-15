813bb905c6a379675507445f2f1aaf50063092bc
carlosquadros-br <carlos.quadros.br@gmail.com>
2023-05-14 23:49:25 -0300

    adicionado partida pontuacao

--- front/src/componentes/fase/FaseGrupo.tsx
+++ front/src/componentes/fase/FaseGrupo.tsx
@@ -1,4 +1,4 @@
-import { Col, Drawer, Row } from "antd";
+import { Button, Col, Drawer, Form, Row } from "antd";
 import { useEffect, useState } from "react";
 import { GrupoData } from "../../datas/GrupoData"
 import { GrupoCard } from "../grupo/GrupoCard";
@@ -6,7 +6,8 @@ import { GrupoTable } from "../grupo/GrupoTable";
 import { getAllPartidasByGrupoId } from "../../servicos/PartidaService";
 import { PartidaData } from "../../datas/PartidaData";
 import { PartidaList } from "../partida/PartidaList";
- 
+import { useAuth } from "../../autenticacao/context/AuthenticationContext";
+
 interface IFaseGrupoProps {
     grupos?: GrupoData[] | null;
 }
@@ -15,11 +16,14 @@ export const FaseGrupo = ({ grupos }: IFaseGrupoProps) => {
     useEffect(() => {
     }, [grupos])
 
+    const { identity } = useAuth();
+
     const [open, setOpen] = useState(false);
     const [partidas, setPartidas] = useState<any[] | null>(null);
+    const [form] = Form.useForm();
 
     const showDrawer = async (grupoId?: number) => {
-        if(!grupoId)
+        if (!grupoId)
             return;
         await getAllPartidasByGrupoId(grupoId).then((partidasData) => setPartidas(partidasData))
         setOpen(true);
@@ -50,8 +54,16 @@ export const FaseGrupo = ({ grupos }: IFaseGrupoProps) => {
                     ))}
                 </Row>
             </div>
-            <Drawer closable={false} onClose={onClose} open={open} width={'50%'}>
-                 <PartidaList partidas={partidas} />
+            <Drawer closable={false} onClose={onClose} open={open} width={'50%'}
+                title="Partidas"
+                extra={
+                    identity.isAdmin ? 
+                    <Button type="primary" onClick={form.submit}>
+                        <span>Salvar</span>
+                    </Button> 
+                    : null
+                }>
+                <PartidaList partidas={partidas} form={form} />
             </Drawer>
         </>
     )
