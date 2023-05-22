2f619a60374d843cc9320867b1e403d452fede5c
Laboratório <carlos.quadros.br@gmail.com>
2023-05-22 20:54:09 -0300

    Refactor ranking

--- back/repository/pontuacao_repository.py
+++ back/repository/pontuacao_repository.py
@@ -1,41 +1,52 @@
-from model import PontuacaoModel
-from model import InscricaoModel, UsuarioModel
+from model import InscricaoModel, UsuarioModel, TorneioModel, PontuacaoModel
 from repository.generic_repository import GenericRepository
-
 
 class PontuacaoRepository(GenericRepository):
     def __init__(self):
         super().__init__(PontuacaoModel)
     
     def get_ranking(self):
-        # Obter todas as inscrições dos usuários
-        inscricoes = InscricaoModel.query.all()
+        # Obter todos os torneios
+        torneios = TorneioModel.query.all()
 
-        # Criar um dicionário de pontuações por usuário
-        pontuacoes = {}
-        for inscricao in inscricoes:
-            usuario_id = inscricao.usuario_id
+        rankings = []  # Lista de rankings por torneio
 
-            # Obter as pontuações do usuário
-            query = self.model.query.filter_by(id_inscricao=inscricao.id)
-            usuario_pontuacoes = query.all()
+        for torneio in torneios:
+            # Obter todas as inscrições do torneio
+            inscricoes = InscricaoModel.query.filter_by(torneio_id=torneio.id).all()
 
-            # Calcular a pontuação total do usuário
-            pontos = sum([pontuacao.pontos for pontuacao in usuario_pontuacoes])
+            # Criar um dicionário de pontuações por usuário para o torneio atual
+            pontuacoes = {}
 
-            # Adicionar a pontuação total ao dicionário de pontuações
-            if usuario_id not in pontuacoes:
-                usuario = UsuarioModel.query.get(usuario_id)
-                pontuacoes[usuario_id] = {
-                    'nome': usuario.nome,
-                    'pontos': pontos,
-                    'clube': usuario.clube,
-                    'federacao': usuario.federacao
-                }
-            else:
-                pontuacoes[usuario_id]['pontos'] += pontos
+            for inscricao in inscricoes:
+                usuario_id = inscricao.usuario_id
 
-        # Ordenar o ranking por pontos
-        ranking = sorted(pontuacoes.values(), key=lambda x: x['pontos'], reverse=True)
+                # Obter as pontuações do usuário para o torneio
+                query = self.model.query.filter_by(id_inscricao=inscricao.id)
+                usuario_pontuacoes = query.all()
 
-        return ranking
+                # Calcular a pontuação total do usuário para o torneio
+                pontos = sum([pontuacao.pontos for pontuacao in usuario_pontuacoes])
+
+                # Adicionar a pontuação total ao dicionário de pontuações
+                if usuario_id not in pontuacoes:
+                    usuario = UsuarioModel.query.get(usuario_id)
+                    pontuacoes[usuario_id] = {
+                        'nome': usuario.nome,
+                        'pontos': pontos,
+                        'clube': usuario.clube,
+                        'federacao': usuario.federacao,
+                    }
+                else:
+                    pontuacoes[usuario_id]['pontos'] += pontos
+
+            # Ordenar o ranking por pontos para o torneio atual
+            ranking_torneio = sorted(pontuacoes.values(), key=lambda x: x['pontos'], reverse=True)
+
+            # Adicionar o ranking do torneio à lista de rankings
+            rankings.append({
+                'torneio': torneio.nome,
+                'ranking': ranking_torneio,
+            })
+
+        return rankings
--- front/src/paginas/ranking/RankingPage.tsx
+++ front/src/paginas/ranking/RankingPage.tsx
@@ -1,73 +1,88 @@
 import { useEffect, useState } from 'react';
 import { Table } from 'antd';
 import axios from 'axios';
-import Title from 'antd/es/skeleton/Title';
+import React from 'react';
 
 interface Ranking {
-  id: number;
   nome: string;
   pontos: number;
   clube: string;
   federacao: string;
 }
 
+interface RankingData {
+  torneio: string;
+  ranking: Ranking[];
+}
+
 export const RankingPage = () => {
-  const [rankings, setRankings] = useState<Ranking[]>([]);
+  const [rankingData, setRankingData] = useState<RankingData[]>([]);
 
   useEffect(() => {
-    const fetchRankings = async () => {
+    const fetchRankingData = async () => {
       try {
-        const response = await axios.get<Ranking[]>('http://localhost:5000/pontuacao/ranking');
-        setRankings(response.data);
+        const response = await axios.get<RankingData[]>('http://localhost:5000/pontuacao/ranking');
+        setRankingData(response.data);
       } catch (error) {
         console.error(error);
       }
     };
 
-    fetchRankings();
+    fetchRankingData();
   }, []);
 
-  const columns = [
-    {
-      title: 'Posição',
-      dataIndex: 'position',
-      key: 'position',
-    },
-    {
-      title: 'Usuário',
-      dataIndex: 'nome',
-      key: 'nome',
-    },
-    {
-      title: 'Clube',
-      dataIndex: 'clube',
-      key: 'clube',
-    },
-    {
-      title: 'Federação',
-      dataIndex: 'federacao',
-      key: 'federacao',
-    },
-    {
-      title: 'Pontuação',
-      dataIndex: 'pontos',
-      key: 'pontos',
-    },
-  ];
+  const renderRankingTable = (torneio: string, ranking: Ranking[]) => {
+    const columns = [
+      {
+        title: 'Posição',
+        dataIndex: 'position',
+        key: 'position',
+      },
+      {
+        title: 'Usuário',
+        dataIndex: 'nome',
+        key: 'nome',
+      },
+      {
+        title: 'Clube',
+        dataIndex: 'clube',
+        key: 'clube',
+      },
+      {
+        title: 'Federação',
+        dataIndex: 'federacao',
+        key: 'federacao',
+      },
+      {
+        title: 'Pontuação',
+        dataIndex: 'pontos',
+        key: 'pontos',
+      },
+    ];
 
-  const data = rankings.map((ranking, index) => ({
-    key: ranking.id,
-    position: index + 1,
-    nome: ranking.nome,
-    clube: ranking.clube,
-    federacao: ranking.federacao,
-    pontos: ranking.pontos,
-  }));
+    const data = ranking.map((rank, index) => ({
+      key: index + 1,
+      position: index + 1,
+      nome: rank.nome,
+      clube: rank.clube,
+      federacao: rank.federacao,
+      pontos: rank.pontos,
+    }));
+
+    return (
+      <div key={torneio}>
+        <h2>Torneio {torneio}</h2>
+        <Table columns={columns} dataSource={data} />
+      </div>
+    );
+  };
 
   return (
     <div>
       <h1>Ranking</h1>
-      <Table columns={columns} dataSource={data} />
+      {rankingData.map((data) =>
+        renderRankingTable(data.torneio, data.ranking)
+      )}
     </div>
   );
 };
