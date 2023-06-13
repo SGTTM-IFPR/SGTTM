de266eb7419ceb9340932e06d01228dd031e3f41
Laboratório <carlos.quadros.br@gmail.com>
2023-06-12 21:13:42 -0300

    Fix query do ranking

--- back/repository/pontuacao_repository.py
+++ back/repository/pontuacao_repository.py
@@ -31,8 +31,7 @@ class PontuacaoRepository(GenericRepository):
                 usuario_id = inscricao.usuario_id
 
                 # Obter as pontuações do usuário para o torneio
-                query = self.model.query.filter_by(id_inscricao=inscricao.id)
-                usuario_pontuacoes = query.all()
+                usuario_pontuacoes = self.model.query.filter_by(id_inscricao=inscricao.id).all()
 
                 # Calcular a pontuação total do usuário para o torneio
                 pontos = sum([pontuacao.pontos for pontuacao in usuario_pontuacoes])
@@ -44,8 +43,8 @@ class PontuacaoRepository(GenericRepository):
                         'nome': usuario.nome,
                         'clube': usuario.clube,
                         'federacao': usuario.federacao,
-                        'ano': torneio.data_inicio
                     }
+                
                 pontuacoes[usuario_id][torneio.nome] = pontos  # Adicionar pontuação do usuário para o torneio atual
 
             # Ordenar o ranking por pontos para o torneio atual
@@ -55,11 +54,10 @@ class PontuacaoRepository(GenericRepository):
             rankings.append({
                 'torneio': torneio.nome,
                 'ranking': ranking_torneio,
-                
             })
 
-            # Adicionar as pontuações do torneio ao dicionário de pontuações por torneio
-            pontuacoes_torneio[torneio.nome] = pontuacoes
+            # Atualizar o dicionário de pontuações por torneio com as pontuações atualizadas
+            pontuacoes_torneio[torneio] = pontuacoes
 
         # Montar o ranking final com as colunas de pontuações por torneio
         ranking_final = []
@@ -78,9 +76,9 @@ class PontuacaoRepository(GenericRepository):
             # Preencher as colunas de pontuações por torneio para o usuário atual
             for torneio, pontuacoes_usuario in pontuacoes_torneio.items():
                 if pontuacoes_usuario.get(usuario.id):
-                    info_usuario[torneio] = pontuacoes_usuario[usuario.id][torneio]
+                    info_usuario[torneio.nome] = pontuacoes_usuario[usuario.id].get(torneio.nome, 0)
                 else:
-                    info_usuario[torneio] = 0
+                    info_usuario[torneio.nome] = 0
 
             ranking_final.append(info_usuario)
 
